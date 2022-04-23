320910f24101a7f9d0a469e227a161e6fc29a157
H <76665736+MeWhenI@users.noreply.github.com>
2022-04-23 00:17:11 -0500

    Make monsters an array
    have all entity collections be arrays instead of monsters being a hash

--- bot.rb
+++ bot.rb
@@ -29,13 +29,13 @@ class Def
  def self.defensive_guy(threats)
   return -1 if threats.size == 0
 
-  return threats.min_by{ |k, v| v.sqdist($my_base_coords) }[0]
+  return threats.min_by{ _1.sqdist($my_base_coords) }.id
  end
 
  def self.defend(monster, hero)
   if monster.sqdist(hero.coords) < 1280**2 && monster.shield_life == 0 && monster.sqdist($my_base_coords) < 2222 **2 && $mana >= 10
    $mana -= 10
-   return "SPELL WIND #{$op_base_x} #{$op_base_y}"
+   return "SPELL WIND #{$op_base_coords.join " "}"
   end
 
   return "MOVE #{monster.coords.join " "}"
@@ -52,11 +52,11 @@ class Att
  def self.attack_guy(monsters)
   return -1 if monsters.size == 0
 
-  return monsters.min_by { |k, v| v.sqdist($op_base_coords) }[0]
+  return monsters.min_by { _1.sqdist($op_base_coords) }.id
  end
 
  def self.att_string(hero, monster_num, monster, base, op_heroes, monsters)
-  if monsters.any?{|k, v| v.sqdist($op_base_coords) < 800**2}
+  if monsters.any?{ _1.sqdist($op_base_coords) < 800**2 }
     def_ops = op_heroes.filter{ _1.sqdist($op_base_coords) < 900**2}
     if def_ops.size > 0
      def_op = def_ops[0]
@@ -99,14 +99,12 @@ end
 
 $mana = 0
 
-$my_base_x, $my_base_y = gets.split.collect &:to_i
-$my_base_coords = [$my_base_x, $my_base_y]
-$op_base_x, $op_base_y = MAP_WIDTH - $my_base_x, MAP_HEIGHT - $my_base_y
-$op_base_coords = [$op_base_x, $op_base_y]
+$my_base_coords = gets.split.collect &:to_i
+$op_base_coords = [MAP_WIDTH - $my_base_coords[0], MAP_HEIGHT - $my_base_coords[1]]
 $home_bases = [
-  [$my_base_x == 0 ? 2000 : MAP_WIDTH - 2000, $my_base_y == 0 ? 2000 : MAP_HEIGHT - 2000],
-  [$my_base_x == 0 ? 5000 : MAP_WIDTH - 5000, $my_base_y == 0 ? 5000 : MAP_HEIGHT - 5000],
-  [$my_base_x != 0 ? 3000 : MAP_WIDTH - 3000, $my_base_y != 0 ? 3000 : MAP_HEIGHT - 3000]
+  [$my_base_coords[0] == 0 ? 2000 : MAP_WIDTH - 2000, $my_base_coords[1] == 0 ? 2000 : MAP_HEIGHT - 2000],
+  [$my_base_coords[0] == 0 ? 5000 : MAP_WIDTH - 5000, $my_base_coords[1] == 0 ? 5000 : MAP_HEIGHT - 5000],
+  [$my_base_coords[0] != 0 ? 3000 : MAP_WIDTH - 3000, $my_base_coords[1] != 0 ? 3000 : MAP_HEIGHT - 3000]
 ]
 $heroes_per_player = gets.to_i
 
@@ -120,15 +118,15 @@ loop {
 
  my_heroes = []
  op_heroes = []
- monsters  = {}
+ monsters  = []
  gets.to_i.times {
   id, type, *stats = gets.split.collect &:to_i
-  type == TYPE_MONSTER && monsters[id]  = Entity.new(id, stats)
+  type == TYPE_MONSTER && monsters     << Entity.new(id, stats)
   type == TYPE_MY_HERO && my_heroes    << Entity.new(id, stats)
   type == TYPE_OP_HERO && op_heroes    << Entity.new(id, stats)
  }
 
- threats = monsters.filter { |k, v| v.threat_for == 1 }
+ threats = monsters.filter { _1.threat_for == 1 }
 
  # Shmoov
 
@@ -136,17 +134,17 @@ loop {
 
  # Defensive guy 0
  hero = my_heroes[0]
- target = Def.defensive_guy(threats.filter{|k, v| v.sqdist($my_base_coords) < 6000**2 })
- puts Def.def_string(hero, target, monsters[target], $under_attack ? $home_bases[0] : jungler_base(-1))
+ target = Def.defensive_guy(threats.filter{ _1.sqdist($my_base_coords) < 6000**2 })
+ puts Def.def_string(hero, target, monsters.find { _1.id == target }, $under_attack ? $home_bases[0] : jungler_base(-1))
  threats.delete(target) if threats.size > 1
 
  # Defensive guy 1
  hero = my_heroes[1]
  target = Def.defensive_guy(threats)
- puts Def.def_string(hero, target, monsters[target], jungler_base(1))
+ puts Def.def_string(hero, target, monsters.find { _1.id == target }, jungler_base(1))
 
  # Attack guy 0
  hero = my_heroes[2]
- target = Att.attack_guy(monsters.filter { |k, v| v.shield_life == 0 && v.sqdist($op_base_coords) < 7000**2 })
- puts Att.att_string(hero, target, monsters[target], $home_bases[2].map {_1 + 2000 - rand(4000)}, op_heroes, monsters)
+ target = Att.attack_guy(monsters.filter { _1.shield_life == 0 && _1.sqdist($op_base_coords) < 7000**2 })
+ puts Att.att_string(hero, target, monsters.find { _1.id == target }, $home_bases[2].map {_1 + 2000 - rand(4000)}, op_heroes, monsters)
 }
